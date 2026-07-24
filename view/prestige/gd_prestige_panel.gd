0e5c478c9084a74ba1094bc95d9160c34055de8d
Manuel Matscher <manuel.matscher@gmail.com>
2026-07-24 18:01:06 +0200

    added the yard addon, which allows for easier editing of resource files

--- view/prestige/gd_prestige_panel.gd
+++ view/prestige/gd_prestige_panel.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	dummy_label.text = App.preview_biomass_gain()._to_string()
 
 func _prestige() -> void:
